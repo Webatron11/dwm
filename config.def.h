a6047d46b83d6dfecd8f52df08d214186893433e
Khorneflaeks <oscarwebb162@protonmail.com>
2021-10-06 10:29:52 +1100

    Chaning lock keypress to prevent clashing

--- config.def.h
+++ config.def.h
@@ -75,7 +75,7 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_s,	   spawn,          {.v = sshcmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,			XK_f,	   spawn,	   {.v = browsercmd} },
-	{ MODKEY,			XK_l,	   spawn,	   {.v = lockcmd } },
+	{ MODKEY,			XK_s,	   spawn,	   {.v = lockcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
